9b5f8890f390ba88beea7e5e85787106ca4601c9
Sitrus1 <sirojiddin2010all@gmail.com>
2025-08-23 23:37:12 +0500

    nothing :(

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,44 +1,3 @@
-// import axios, { AxiosInstance } from "axios";
-
-// const api: AxiosInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_APPLE,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export const registerStep1 = async (data: { email: string; password: string; confirm_password: string }) => {
-//   return api.post("/accounts/auth/signup/", data);
-// };
-
-// export const verifyEmail = async (data: { code: string }) => {
-//   console.log("Yuborilayotgan so‘rov:", data);
-//   return api.post("/accounts/auth/email-verify/", data);
-// };
-
-// export const signIn = async ( data: { email: string; password: string; }) => {
-//   return api.post("/accounts/auth/signin/", data);
-// };
-
-// export const getProfile = async (token: string) => {
-//   console.log("GET so‘rov headeri:", { Authorization: `Bearer ${token}` });
-//   return api.get("/accounts/account/profile/", {
-//     headers: { Authorization: `Bearer ${token}` },
-//   });
-// };
-
-// export const forgotPassword = async (data: { email: string }) => {
-//   console.log("Forgot password so‘rovi:", data);
-//   return api.post("/accounts/auth/forgot-password/", data);
-// };
-
-// export const resetPassword = async (token: string, data: { new_password: string; confirm_password: string }) => {
-//   console.log("Reset password so‘rovi:", { token, data });
-//   return api.post(`/accounts/auth/reset-password/${token}/`, data);
-// };
-
-
-
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
@@ -149,6 +108,13 @@ export const searchUsers = async (key: string, token: string) => {
 
 export const uploadFile = async (data: FormData, token: string) => {
   return api.post("/chat/upload/", data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export const patchLocation = async (token: string, data: { lat: string; long: string }) => {
+  console.log("Patch location so‘rovi:", { token, data });
+  return api.patch("/accounts/account/location/", data, {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
